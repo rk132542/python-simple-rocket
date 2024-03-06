5d4acfd38af96a1aa5f8443cc2faec17facf1f59
rk132542 <118442940+rk132542@users.noreply.github.com>
2024-03-06 19:13:23 +0100

    Add thrust level and engine on/off functionality

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ class Rocket:
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.color = (128, 128, 128)
         self.mass = 1500  # in kg
+        self.thrust_level = 0 # 0~1 -> 0~100%
+        self.engine_on = False
 
     # Draw the rocket
     def draw(self, win):
@@ -57,6 +59,13 @@ def draw_bg():
     for star in stars:
         pygame.draw.circle(WIN, (255, 255, 255), (star[0], star[1]), 1)
 
+def draw_thrust_level(thrust_level, engine_on):
+    gauge_color = (255, 0, 0)
+    if not engine_on:
+        gauge_color = (128, 128, 128)
+    pygame.draw.rect(WIN, (255, 255, 255), (10, 10, 100, 20))
+    pygame.draw.rect(WIN, gauge_color, (10, 10, 100 * thrust_level, 20))
+
 
 def init_stars():
     for _ in range(100):
@@ -68,9 +77,20 @@ def init_stars():
 GRAVITY = 1.625 / 60  # m/s
 
 
-def draw_fire(x, y):
-    pygame.draw.rect(WIN, (255, 0, 0), (x+5, y + rocket.height, rocket.width-10, 10))
-    pygame.draw.polygon(WIN, (255, 0, 0), [(x+rocket.width//2, y+rocket.height+10), (x+rocket.width//2-5, y+rocket.height+20), (x+rocket.width//2+5, y+rocket.height+20)])
+def draw_fire(x, y, thrust_level):
+    flame_height = int(thrust_level * 20)
+    flame_width = rocket.width - 10
+    flame_x = x + 5
+    flame_y = y + rocket.height
+    flame_color = (255, 0, 0)
+    
+    
+    if thrust_level > 0:
+        pygame.draw.rect(WIN, flame_color,
+                         (flame_x, flame_y, flame_width, flame_height))
+        pygame.draw.polygon(WIN, flame_color, [(x + rocket.width // 2, y + rocket.height + flame_height),
+                                               (x + rocket.width // 2 - 5, y + rocket.height + flame_height + 10),
+                                               (x + rocket.width // 2 + 5, y + rocket.height + flame_height + 10)])
 
 
 def main():
@@ -92,19 +112,21 @@ def main():
             rocket.vel[1] = 0
             rocket.y = HEIGHT - rocket.height
             
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
+        if rocket.engine_on:
             # 2700 N thrust
             # F = ma
             # a = F/m
             # a = 2700/1500
             # a = 1.8
-            rocket.vel[1] -= 1.8 / 60
+            thrust = 1.8 / 60
+            rocket.vel[1] -= (thrust * rocket.thrust_level)
 
-            draw_fire(rocket.x, rocket.y)
+            draw_fire(rocket.x, rocket.y, rocket.thrust_level)
 
         rocket.update()
         rocket.draw(WIN)
+
+        draw_thrust_level(rocket.thrust_level, rocket.engine_on)
 
         # Update the display
         pygame.display.update()
@@ -112,6 +134,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    rocket.engine_on = not rocket.engine_on
+            
+            elif event.type == pygame.MOUSEWHEEL:
+                # precise_y -> scroll
+                print(event.y) # -10 to 10
+                rocket.thrust_level += -1 * event.y / 100
+                rocket.thrust_level = max(0, rocket.thrust_level)
+                rocket.thrust_level = min(1, rocket.thrust_level)
 
     pygame.quit()
 
