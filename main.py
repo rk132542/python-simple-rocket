3ea9dd4f0d01870c364dfdcc5ecfcae14335009b
rk132542 <118442940+rk132542@users.noreply.github.com>
2024-03-06 10:33:10 +0100

    added basic comments

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ pygame.init()
 WIN = pygame.display.set_mode((800, 500))
 pygame.display.set_caption("Game")
 
+# Rocket class to create a rocket object
 class Rocket:
     def __init__(self, x, y, width, height):
         self.x = x
@@ -15,6 +16,7 @@ class Rocket:
         self.acc = [0, 0]
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         
+    # Draw the rocket
     def draw(self, win):
         pygame.draw.rect(win, (255, 0, 0), self.rect)   
         
@@ -28,10 +30,12 @@ def main():
     run = True
     while run:
         clock.tick(60)
+        
+        # Clear the screen
         WIN.fill((255, 255, 255))
-        
+        # Draw the rocket
         rocket.draw(WIN)
-        
+        # Update the display
         pygame.display.update()
         
         for event in pygame.event.get():
